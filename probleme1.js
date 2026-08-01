1bf92b233a865b8915d95d59f9d370dd679b5056
Amine <aminedhib40@gmail.com>
2026-08-01 10:41:57 +0100

    prob1

--- probleme1.js
+++ probleme1.js
@@ -1,6 +1,12 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
 
+function nomprenom (nom,prenom){
 
+return nom+' '+prenom
+
+
+}
+console.log(nomprenom("Amine","dhib"))
 
 
 
@@ -10,6 +16,10 @@
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
 
+function moyenne(nbr1,nbr2){
+    return (nbr1+nbr2)/2
+}
+console.log(moyenne(2,4))
 
 
 
@@ -20,8 +30,11 @@
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
 
+function nbrlength(chain){
+return chain.length
 
-
+}
+console.log(nbrlength("bonjour"))
 
 
 
@@ -30,7 +43,12 @@
 
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
 
+function souschaine(chaine,position){
 
+return chaine.slice(position)
+
+}
+console.log(souschaine("Anine",1))
 
 
 
