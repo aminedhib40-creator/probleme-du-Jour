bdd812d60668e20ee8e5260f98904eb11273a50a
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-08-08 08:00:48 +0100

    initial commit

---
+++ probleme3.js
@@ -0,0 +1,12 @@
+
+
+// 1- Écris une fonction factorielleN qui calcule la factorielle de n avec une boucle while.
+
+
+// 2- Écris une fonction afficheMultiplesDe3 qui prend un nombre n et affiche tous les multiples de 3 de 0 jusqu’à n.
+
+
+// 3- Crée une fonction qui prend un nombre et affiche "Bonjour" ce nombre de fois avec while.
+
+
+// 4- Écris une fonction compteVoyelles qui prend une chaîne de caractères et compte le nombre de voyelles avec une boucle while.
